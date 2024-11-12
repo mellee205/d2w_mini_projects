1caafc22724c90fbdc3b31a54d1c95d5a7ecaa8b
mellee205 <1007933@mymail.sutd.edu.sg>
2024-11-13 00:57:24 +0800

    Adding all Edits (no new files)

--- mp_calc/app/serverlibrary.py
+++ mp_calc/app/serverlibrary.py
@@ -1,13 +1,169 @@
+def merge(array,p,q,r,byfunc):
+    len_left=q-p+1
+    len_right=r-q
+    left_array=array[p:q+1]
+    right_array=array[q+1:r+1]
+    left=0
+    right=0
+    target=p
 
+    if byfunc!=None:
+      command=byfunc
+      while (left<len_left) and (right<len_right):
+          if command(left_array[left])<=command(right_array[right]):
+              array[target]=left_array[left]
+              left+=1
+          else:
+              array[target]=right_array[right]
+              right+=1
+          target+=1
+    else:
+      while (left<len_left) and (right<len_right):
+        if left_array[left]<=right_array[right]:
+            array[target]=left_array[left]
+            left+=1
+        else:
+            array[target]=right_array[right]
+            right+=1
+        target+=1
+    while (left<len_left):
+        array[target]=left_array[left]
+        left+=1
+        target+=1
+    while (right<len_right):
+        array[target]=right_array[right]
+        right+=1
+        target+=1
+
+def mergesort_recursive(array, p, r,byfunc):
+  if r-p>0:
+    q=(p+r)//2
+    mergesort_recursive(array,p,q,byfunc)
+    mergesort_recursive(array,q+1,r,byfunc)
+    merge(array,p,q,r,byfunc)
 
 def mergesort(array, byfunc=None):
-  pass
+  mergesort_recursive(array,0,len(array)-1,byfunc)
 
 class Stack:
-  pass
+    def __init__(self)->None:
+        self._items=[]
+    
+    def push(self,item):
+        self._items.append(item)
+    
+    def pop(self):
+        if len(self._items)==0:
+            return None
+        else:
+            last_item=self._items[-1]
+            self._items=self._items[:-1]
+            return last_item
+    
+    def peek(self):
+        if len(self._items)==0:
+            return None
+        else:
+            return self._items[-1]
+    
+    @property
+    def is_empty(self)->bool:
+        if len(self._items)==0:
+            return True
+        else:
+            return False
+    
+    @property
+    def size(self)->int:
+        return len(self._items)
+    
+    @property
+    def _Stack__items(self):
+        return self._items
+    
+    @_Stack__items.setter
+    def _Stack__items(self,value):
+        self._items=value
 
 class EvaluateExpression:
-  pass
+  valid_char = '0123456789+-*/() '
+  def __init__(self, string=""):
+    self.expr=""
+    self.expression=string
+
+  @property
+  def expression(self):
+    return self.expr
+
+  @expression.setter
+  def expression(self, new_expr):
+    #this somehow also handles "" but im not complaining
+    is_valid=True
+    for ch in new_expr:
+      if ch not in self.valid_char:
+        is_valid=False
+    if is_valid:
+      self.expr=new_expr
+    else:
+      self.expr=""
+
+  def insert_space(self):
+    final_str=""
+    operators='+-*/()'
+    operands="1234567890"
+    for ch in self.expression:
+      if ch in operators:
+        final_str+=f" {ch} "
+      elif ch in operands:
+        final_str+=ch
+    return final_str
+  
+  def process_operator(self, operand_stack, operator_stack):
+    second=operand_stack.pop()
+    first=operand_stack.pop()
+    operator=operator_stack.pop()
+
+    if operator=="/":
+      operand_stack.push(int(first)//int(second))
+    else:
+      #print(str(first))
+      #print(operator)
+      #print(str(second))
+      operand_stack.push(eval(str(first)+operator+str(second)))  
+
+  def evaluate(self):
+    operand_stack = Stack()
+    operator_stack = Stack()
+    expression = self.insert_space()
+    tokens = expression.split()
+    #print(tokens)
+    operands="1234567890"
+
+    for ch in tokens:
+      #print(ch)
+      #print("operand",operand_stack._Stack__items)
+      #print("operator",operator_stack._Stack__items)
+      if ch in operands:
+        operand_stack.push(ch)
+      elif ch=="+" or ch=="-":
+        while operator_stack.size!=0 and operator_stack.peek()!="(" and operator_stack.peek()!=")":
+          self.process_operator(operand_stack,operator_stack)
+        operator_stack.push(ch)
+      elif ch=="*" or ch=="/":
+        while operator_stack.size!=0 and (operator_stack.peek()=="*" or operator_stack.peek()=="/"):
+          self.process_operator(operand_stack,operator_stack)
+        operator_stack.push(ch)
+      elif ch=="(":
+        operator_stack.push(ch)
+      elif ch==")":
+        while operator_stack!=0 and operator_stack.peek()!="(":
+          self.process_operator(operand_stack,operator_stack)
+        if operator_stack.peek()=="(":
+          operator_stack.pop()
+    while operator_stack.size!=0:
+      self.process_operator(operand_stack,operator_stack)
+    
+    return operand_stack.pop()
 
 
 def get_smallest_three(challenge):
--- mp_sort/app/static/library.py
+++ mp_sort/app/static/library.py
@@ -2,7 +2,11 @@ from org.transcrypt.stubs.browser import *
 import random
 
 def gen_random_int(number, seed):
-	pass
+	random.seed(seed)
+	random_no=[]
+	for i in range(number):
+		random_no.append(random.randint(0,number))
+	return random_no
 
 def generate():
 	number = 10
@@ -12,18 +16,32 @@ def generate():
 	# store it to the variable array
 	pass
 
-	array = None
+	array = gen_random_int(number,seed)
 	# convert the items into one single string 
 	# the number should be separated by a comma
 	# and a full stop should end the string.
 	pass
 
-	array_str = None
-
+	array_str = ""
+	for num in array:
+		array_str+=str(num)
+		array_str+=","
+	array_str=array_str[:-1]+'.'
+	
 	# This line is to placed the string into the HTML
 	# under div section with the id called "generate"	
 	document.getElementById("generate").innerHTML = array_str
 
+def insertion_sort2(array:list[int|float])->list[int|float]:
+    length=len(array)
+    for outer in range(1,length):
+        target=array[outer]
+        target_idx=outer
+        for inner in range(outer,0,-1):
+            if array[inner-1]>target:
+                target_idx-=1
+        array.pop(outer)
+        array.insert(target_idx,target)
 
 def sortnumber1():
 	'''	This function is used in Exercise 1.
@@ -35,9 +53,21 @@ def sortnumber1():
 		- call your sort function, either bubble sort or insertion sort
 		- create a string of the sorted numbers and store it in array_str
 	'''
-	pass
+	unsorted_str=document.getElementById("generate").innerHTML
+	unsorted_str=unsorted_str[0:-1]
+	unsorted_list=unsorted_str.split(',')
 
-	array_str = None
+	int_list=[int(item)for item in unsorted_list]
+
+	insertion_sort2(int_list)
+
+	array_str = ""
+	for num in int_list:
+		array_str+=str(num)
+		array_str+=","
+	array_str=array_str[:-1]+'.'
+	
+	pass
 	
 	document.getElementById("sorted").innerHTML = array_str
 
@@ -60,12 +90,54 @@ def sortnumber2():
 		window.alert("Your textbox is empty")
 		return
 
+	accepted_characters=[',','.','1','2','3','4','5','6','7','8','9','0','-']
+	for character in value:
+		if character not in accepted_characters:
+			window.alert("You have entered an invalid number")
+			return
 	# Your code should start from here
 	# store the final string to the variable array_str
+	unsorted_list=value.split(',')
+
+	def count(number,symbol):
+		symbol_count=0
+		for ch in number:
+			if ch==symbol:
+				symbol_count+=1
+		return symbol_count
+
+	int_list=[]
+	skipped=[]
+
+	for number in unsorted_list:
+		if "." in number or '-' in number:
+			valid=True
+			if count(number,'.')>1:
+				valid=False
+			elif '-' in number and (count(number,'-')>1 or number[0]!='-'):
+				valid=False
+			
+			if valid:
+				int_list.append(float(number))
+			else:
+				skipped.append(number)
+
+		elif number!="":
+			int_list.append(float(number))
+
+	insertion_sort2(int_list)
+
+	array_str = ""
+	for num in int_list:
+		array_str+=str(num)
+		array_str+=","
+	array_str=array_str[:-1]+'.'
+
 	pass
 
-	array_str = None
-
 	document.getElementById("sorted").innerHTML = array_str
+	if skipped!=[]:
+		window.alert(f"Code skipped {skipped}.")
+
 
 
